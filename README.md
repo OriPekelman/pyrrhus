df5a89776eff91caca63aae73f7e9449f6cb0cd3
Ori Pekelman <ori@pekelman.com>
2019-04-15 12:30:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# Pyrrhus - Universal One-Click Hyperscale installer
+
+Currently only support Mac OS X as it is the only hyperscale operating system.  And your PowerBook Duo is just like the cloud.
+
+Installs, Docker, Kubernetes, Rust, Ruby, NodeJS, Golang something called kubedb, an `sh` installer, and potentially many other things; But how would I know?
+
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. Deployment is precisely the same thing so you should not worry. Not one bit.
+
+### Prerequisites
+
+A command-line.
+
+### Installing
+
+```
+curl -sf -L https://raw.githubusercontent.com/OriPekelman/pyrrhus/master/install.sh | sudo sh
+```
+
+
+## Running the tests
+
+There are no tests, because testing is doubting and you do trust me.
+
+### Coding style
+
+Yolo, advanced edition.
+
+## Deployment
+
+I told you, its just like development. 
+
+```
+curl -sf -L https://raw.githubusercontent.com/OriPekelman/pyrrhus/master/install.sh | sudo sh
+```
+
+## Built With
+
+All the things.
+
+## Contributing
+
+Please don't. You have a day job.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Ori Pekelman** - *Initial work* - [Ori Pekelman](https://github.com/oripekelman)
+
+See also the list of [contributors](https://github.com/oripekelman/pyrrhus/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the UniLicense - see the [LICENSE](LICENSE) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone who created a | sh installer and made developers lives easier;
+* Thanks to the Joomla community who have created a 22kb .gitignore used for extra cloudiness in this project.
+* Warm regards to my colleagues at https://platform.sh that allow me not roll my own infrastructure on every project which makes me feel like an adult. Which I rarely do.
---
+++ install.sh
@@ -0,0 +1,25 @@
+if [ -z ${YOLO} ]
+then
+  ##
+  # This is a joke. Please don't run this. None of the below commands is
+  # willfully harmful; all have been copy and pasted from somewhere reputable on
+  # the interwebs, but this is a very bad idea. A very bad one. 
+  #
+  # Personally, I did not try to run this, and I never will.
+  ##
+  echo "Congratulations. You are an adult; or lucky, or you don't know how to set an environment variable. Which all considering, is good. Because you should really not be running this."
+else
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+  softwareupdate -i $(softwareupdate -l | grep "\*.*Command Line" | grep $(sw_vers -productVersion) | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n') --verbose
+  gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  curl -s https://i.jpillora.com/installer | bash
+  base=https://github.com/docker/machine/releases/download/v0.16.0 && curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/usr/local/bin/docker-machine && chmod +x /usr/local/bin/docker-machine
+  curl -sf -L https://static.rust-lang.org/rustup.sh | sh
+  curl -sSL https://get.rvm.io | sh
+  curl -O https://storage.googleapis.com/golang/getgo/installer_darwin && chmod +x installer_darwin && ./installer_darwin
+  curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" &&  installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+  curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64  && chmod +x minikube
+  wget -O kubedb https://github.com/kubedb/cli/releases/download/0.8.0-beta.2/kubedb-darwin-amd64  && chmod +x kubedb   &&  mv kubedb /usr/local/bin/
+  rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+fi
